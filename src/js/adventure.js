e550cfa56141de6a8aa0946f161cff6702262c3b
Ing. Miroslav Biňas PhD <miroslav.binas@tuke.sk>
2018-05-22 14:30:28 +0200

    koniec 3. tyzdna

--- src/js/adventure.js
+++ src/js/adventure.js
@@ -1,10 +1,43 @@
 let main = $('main');
 let input = $('input');
 
-main.append('<p>Nachadzas sa v skoliacej miestnosti Anton Bernolak (bez Antona Bernolaka). Volne pohodene pocitace a svietiaci projektor nevestia nic dobre.</p>');
+function showRoom(room){
+    main.append('<p>');
+    main.append(room.description + '</br>');
+    main.append('Možné východy:');
+    main.append('<ul>');
+    for(let i = 0; i < room.exits.length; i++){
+        main.append('<li>' + room.exits[i] + '</li>');
+    }
+    main.append('</ul></p>');
+}
 
-$('input').on('keyup', function(event){
-    if(event.keyCode === 13){
+let world = {
+    bernolak: {
+        name: 'bernolak',
+        description: 'Nachadzas sa v skoliacej miestnosti Anton Bernolak (bez Antona Bernolaka). Volne pohodene pocitace a svietiaci projektor nevestia nic dobre.',
+        exits: ['kuchynka']
+    },
+
+    kuchynka: {
+        name: 'kuchynka',
+        description: 'Vitaj v kuchynke neobmedzenych v mikrovlnke pripravovanych pochutin. Ti narocnejsi si mozu vybrat aj nieco z dnesnej ponuky chladnicky.',
+        exits: ['bernolak', 'toalety']
+    },
+
+    toalety: {
+        name: 'toalety',
+        description: 'Vošiel si do pánskych toaliet. Do nosa ti vrazila opojná vôňa osivežujúceho prostriedku. "Už nikdy inde", pomyslel si si.',
+        exits: ['kuchynka']
+    }
+}
+
+let currentRoom = 'bernolak';
+
+showRoom(world[currentRoom]);
+
+$('input').on('keyup', function (event) {
+    if (event.keyCode === 13) {
         let command = input.val().trim();
         main.append('<p>&gt; ' + command + '</p>');
 
@@ -13,19 +46,20 @@ $('input').on('keyup', function(event){
                 main.append('<p>Tuto mocnu pecku vytvoril peckovy javastripter mire(c) z kosi(c) 2018.</p>');
                 break;
             }
-        
-            case 'KUCHYNKA': {
-                main.append('<p>Vitaj v kuchynke neobmedzenych v mikrovlnke pripravovanych pochutin. Ti narocnejsi si mozu vybrat aj nieco z dnesnej ponuky chladnicky.</p>');
+
+            case 'ROZHLIADNI SA': {
+                showRoom(world[currentRoom]);
                 break;
             }
-        
-            case 'BERNOLAK': {
-                main.append('<p>Nachadzas sa v skoliacej miestnosti Anton Bernolak (bez Antona Bernolaka). Volne pohodene pocitace a svietiaci projektor nevestia nic dobre.</p>');
-                break;
-            }
-        
+
             default: {
-                main.append('<p>Tam sa neda ist.</p>');
+                command = command.toLowerCase();
+                if(world[currentRoom].exits.indexOf(command) > -1){
+                    currentRoom = command;
+                    showRoom(world[currentRoom]);
+                }else{
+                    main.append('<p>Taký príkaz nepoznám.</p>');
+                }
             }
         }
 
