0d7609233fbccebf56aee6142c7b1c3c3b757bd8
Ing. Miroslav Biňas PhD <miroslav.binas@tuke.sk>
2018-07-10 20:14:06 +0200

    podpora pre ukladanie udajov na back4app
    * oprava index.html - pouzitie jquery s podporou ajax-u
    * upravene prikazy pre ulozenie a nacitanie historie
        * pracuje s ajax-om
        * ukalada na back4app

--- src/js/commands/load.js
+++ src/js/commands/load.js
@@ -15,37 +15,71 @@ export class Load extends Command {
             return;
         }
 
+        const savepoint = this._param;
+
         // if no such savepoint, then quit
-        if (this._param in localStorage == false) {
-            main.append("Savepoint s týmto názvom neexistuje.</br>");
-            return;
-        }
+        // if (this._param in localStorage == false) {
+        //     main.append("Savepoint s týmto názvom neexistuje.</br>");
+        //     return;
+        // }
 
         // load
-        const value = localStorage.getItem(this._param);
-        const history = JSON.parse(value);
+        // const value = localStorage.getItem(this._param);
+        // const history = JSON.parse(value);
 
-        // reset
-        game.initGame();
-        const parser = new Parser();
-
-        // process
-        for (let line of history) {
-            const cmd = parser.parse(line);
-
-            if (cmd === null) {
-                line = line.toLowerCase();
-                // save to history first
-                game.history.push(line);
-
-                // move
-                game.currentRoom = game.world[line];
-            } else {
-                cmd.exec(game);
+        // load from back4app
+        var settings = {
+            async: true,
+            crossDomain: true,
+            url: `https://parseapi.back4app.com/classes/history?where={"save":"${savepoint}"}`,
+            method: "GET",
+            headers: {
+                "X-Parse-Application-Id":
+                    "B2eLnipNaHfcbzMVYIY5vK0plPRaTu6uOytfzhvQ",
+                "X-Parse-REST-API-Key":
+                    "lqQh5N21jgpiy9CzUsZqg5SzaJSnzSAkMTJr76fo"
             }
-        }
+        };
 
-        game.currentRoom.showRoom();
-        main.append("Hra bola úspešne načítaná.</br>");
+        $.ajax(settings)
+            .done(function(response) {
+                const results = response["results"];
+
+                if (results["length"] == 0) {
+                    main.append("Taký savepoint neexistuje.</br>");
+                    return;
+                }
+
+                // parse history
+                const history = JSON.parse(response["results"][0]["history"]);
+
+                // reset
+                game.initGame();
+                const parser = new Parser();
+
+                // process
+                for (let line of history) {
+                    const cmd = parser.parse(line);
+
+                    if (cmd === null) {
+                        line = line.toLowerCase();
+                        // save to history first
+                        game.history.push(line);
+
+                        // move
+                        game.currentRoom = game.world[line];
+                    } else {
+                        cmd.exec(game);
+                    }
+                }
+
+                game.currentRoom.showRoom();
+                main.append("Hra bola úspešne načítaná.</br>");
+            })
+            .fail(function(response) {
+                console.log(response.status);
+                console.log(response.responseText);
+                main.append("Hru sa nepodarilo načítať.</br>");
+            });
     }
 }
--- src/js/commands/save.js
+++ src/js/commands/save.js
@@ -14,16 +14,47 @@ export class Save extends Command {
             return;
         }
 
-        // if such savepoint exists, then quit
-        if (this._param in localStorage) {
-            main.append("Savepoint s týmto názvom už existuje.</br>");
-            return;
-        }
+        // localStorage - if such savepoint exists, then quit
+        // if (this._param in localStorage) {
+        //     main.append("Savepoint s týmto názvom už existuje.</br>");
+        //     return;
+        // }
 
-        // save
-        const history = JSON.stringify(game.history);
-        localStorage.setItem(this._param, history);
+        // localStorage - save
+        // const history = JSON.stringify(game.history);
+        // localStorage.setItem(this._param, history);
+        // main.append("Hra bola úspešne uložená.</br>");
 
-        main.append("Hra bola úspešne uložená.</br>");
+        // back4app - if such savepoint exists, then quit
+
+        // back4app - save
+        var settings = {
+            async: true,
+            crossDomain: true,
+            url: "https://parseapi.back4app.com/classes/history",
+            method: "POST",
+            headers: {
+                "X-Parse-Application-Id":
+                    "B2eLnipNaHfcbzMVYIY5vK0plPRaTu6uOytfzhvQ",
+                "X-Parse-REST-API-Key":
+                    "lqQh5N21jgpiy9CzUsZqg5SzaJSnzSAkMTJr76fo",
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                save: this._param,
+                history: JSON.stringify(game.history)
+            })
+        };
+
+        $.ajax(settings)
+            .done(function(response) {
+                console.log("Hra bola uspesne ulozena aj na back4app.");
+                main.append("Hra bola úspešne uložená.</br>");
+            })
+            .fail(function(response) {
+                console.log(response.status);
+                console.log(response.responseText);
+                main.append("Hru sa nepodarilo uložiť.</br>");
+            });
     }
 }
